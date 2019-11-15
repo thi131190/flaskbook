2a42244d2d13196a5a93f21041054c71f1b13d5d
thi131190 <thi131190@gmail.com>
2019-11-15 13:15:33 +0700

    config flask app and libraries

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissecret'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskbook.db'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
